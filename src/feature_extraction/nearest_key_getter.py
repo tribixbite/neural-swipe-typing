bfcaca116a85ddc73b23b82435f5e9ac96ff8459
Garry Proshian <98213116+proshian@users.noreply.github.com>
2025-05-16 16:31:34 +0300

    Add NearestKeyGetter

---
+++ src/feature_extraction/nearest_key_getter.py
@@ -0,0 +1,18 @@
+from typing import Set, Optional
+
+from torch import Tensor
+
+from .distance_getter import DistanceGetter
+
+
+class NearestKeyGetter:
+    def __init__(self,
+                 grid: dict,
+                 tokenizer,
+                 key_labels: Optional[Set[str]] = None,
+                 ) -> None:
+        self.distance_getter = DistanceGetter(grid, tokenizer, key_labels, 
+                                              fill_missing_distance_val=float('inf'))
+
+    def __call__(self, coords: Tensor) -> Tensor:
+        return self.distance_getter(coords).argmin(dim=1).view(-1, 1)
