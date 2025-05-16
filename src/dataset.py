4e78b9b3ee1e91942710faa594c7251a4eb55768
Garry Proshian <98213116+proshian@users.noreply.github.com>
2025-05-17 00:17:22 +0300

    Update dataset for new feature extraction

--- src/dataset.py
+++ src/dataset.py
@@ -1,13 +1,17 @@
 import json
-from typing import Optional, List, Tuple, Callable
+from collections.abc import Callable
+from typing import Optional, List, Tuple, Dict
 import array
 from multiprocessing import Pool
 
-
 import torch
+from torch import Tensor
 from torch.utils.data import Dataset
 from tqdm import tqdm
 from torch.nn.utils.rnn import pad_sequence
+
+from feature_extraction.swipe_feature_extractors import SwipeFeatureExtractor
+from ns_tokenizers import CharLevelTokenizerv2
 
 
 RawDatasetEl = Tuple[array.array, array.array, 
@@ -28,29 +32,16 @@ def _get_data_from_json_line(line) -> RawDatasetEl:
     return X, Y, T, grid_name, tgt_word
 
 
-class CurveDataset(Dataset):
+class SwipeDataset(Dataset):
     """
     Dataset class for NeuroSwipe jsonl dataset
-    
-    if `init_transform` and `get_item_transform` are None, 
-    curve_dataset_obj[i] is a tuple (X, Y, T, grid_name, tgt_word)
-    If there is no 'word' property in .json file, `tgt_word` is None.
-
-    Transforms are separated into two parts: 
-    * `init_transform` - takes raw data and returns semi-extracted features 
-    * `get_item_transform` takes semi-extracted features and returns 
-        (model_input, target).
-    
-    If `init_transform` is a full transform, the dataset may take too much memory.
-    If `get_item_transform` is a full transform, iterating over 
-        the dataset may be slow. 
     """
 
     def __init__(self,
                  data_path: str,
                  store_gnames: bool,
-                 init_transform: Optional[Callable] = None,
-                 get_item_transform: Optional[Callable] = None,
+                 word_tokenizer: CharLevelTokenizerv2,
+                 grid_name_to_swipe_feature_extractor: Dict[str, SwipeFeatureExtractor],
                  total: Optional[int] = None):
         """
         Arguments:
@@ -71,23 +62,18 @@ class CurveDataset(Dataset):
                 - grid_name (str): name of the keyboard grid.
         store_gnames: bool
             If True, stores grid names in self.grid_name_list.
-        init_transform: Optional[Callable]
-            A function that takes raw data (X, Y, T, grid_name, tgt_word)
-            and returns semi-extracted features.
-        get_item_transform: Optional[Callable]
-            A function that takes semi-extracted features and returns 
-            (model_input, target).
         total: Optional[int]
             Number of dataset elements. Is used only for progress bar.
         """
-        self.transform = get_item_transform
         self.data_list = self._get_data(
-            data_path, init_transform, store_gnames, total)
+            data_path, store_gnames, total=total)
+        self.word_tokenizer = word_tokenizer
+        self.grid_name_to_swipe_feature_extractor = grid_name_to_swipe_feature_extractor
         
     def _get_data(self,
                   data_path: str,
-                  transform: Optional[Callable],
                   set_gnames: bool,
+                  transform: Optional[Callable] = None,
                   total: Optional[int] = None) -> List[RawDatasetEl]:
         data_list = []
         if set_gnames:
@@ -107,28 +93,47 @@ class CurveDataset(Dataset):
                                  ) -> RawDatasetEl:
         return _get_data_from_json_line(line)
     
+    def _get_decoder_in_and_out(self, tgt_word: str
+                                ) -> Tuple[Tensor, Tensor]:
+        tgt_token_seq: List[int] = self.word_tokenizer.encode(tgt_word)
+        tgt_token_seq = torch.tensor(tgt_token_seq, dtype=self.dtype)
+        decoder_in = tgt_token_seq[:-1]
+        decoder_out = tgt_token_seq[1:]
+        return decoder_in, decoder_out
+    
     def __len__(self):
         return len(self.data_list)
     
-    def __getitem__(self, idx):
-        sample = self.data_list[idx]  # X, Y, T, grid_name, tgt_word
-        if self.transform:
-            sample = self.transform(sample)
-        return sample
+    def __getitem__(self, idx: int
+                    ) -> Tuple[Tuple[List[Tensor], Tensor], Tensor]:
+        x, y, t, grid_name, tgt_word = self.data_list[idx]
+        x, y, t = map(
+            lambda x: torch.tensor(x, dtype=torch.float32), 
+            (x, y, t))
+        swipe_feature_extractor = self.grid_name_to_swipe_feature_extractor[grid_name]
+        swipe_features = swipe_feature_extractor(x, y, t)
+        decoder_in, decoder_out = self._get_decoder_in_and_out(tgt_word)
+        
+        return ((swipe_features, decoder_in), decoder_out)
     
     @classmethod
     def from_data_list(cls, 
                        data_list: list, 
+                       word_tokenizer: CharLevelTokenizerv2,
+                       grid_name_to_swipe_feature_extractor: Dict[str, SwipeFeatureExtractor],
                        grid_name_list: Optional[List[str]] = None,
-                       get_item_transform: Optional[Callable] = None,
                        ):
         if grid_name_list:
-            assert len(grid_name_list) == len(data_list)
+            if len(grid_name_list) != len(data_list):
+                raise ValueError(
+                    f"grid_name_list length {len(grid_name_list)} " \
+                    f"doesn't match data_list length {len(data_list)}")
         
         obj = cls.__new__(cls)
 
         obj.data_list = data_list
-        obj.transform = get_item_transform
+        obj.grid_name_to_swipe_feature_extractor = grid_name_to_swipe_feature_extractor
+        obj.word_tokenizer = word_tokenizer
 
         if grid_name_list:
             obj.grid_name_list = grid_name_list
@@ -136,58 +141,9 @@ class CurveDataset(Dataset):
         return obj
 
 
-class CurveDatasetWithMultiProcInit(CurveDataset):
-    def __init__(self,
-                 data_path: str,
-                 store_gnames: bool,
-                 init_transform: Optional[Callable] = None,
-                 get_item_transform: Optional[Callable] = None,
-                 n_workers: int = 0,
-                 total: Optional[int] = None):
-        """
-        Arguments:
-        ----------
-        **All arguments from CurveDatase are present and are same**.
-        n_workers: int
-            If `n_workers` > 0, dataset creation will be parallelized.
-        """
-        self.n_workers = n_workers
-        self.transform = get_item_transform
 
-        get_data_fn = self._get_data_mp if n_workers > 0 else self._get_data
-        self.data_list = get_data_fn(data_path, init_transform, 
-                                     store_gnames, total)
-        
-    def _get_data_mp(self,
-                    data_path: str,
-                    transform: Optional[Callable],
-                    set_gnames: bool,
-                    total: Optional[int] = None) -> List[RawDatasetEl]:
-        data_list = []
-        if set_gnames:
-            self.grid_name_list = []
-        with open(data_path, "r", encoding="utf-8") as json_file:
-            with Pool(self.n_workers) as executor:
-                # Seems like choosing proper chunk size is crucial for efficiency.
-                # Seems like splitting the file into portions leads to overhead.
-                # Note that processign speeds up a lot after around 10 minutes. 
-                n_chunks_per_workser = 8
-                chunksize = int(total / n_chunks_per_workser / self.n_workers)
-                # cuncurrent.futures.PoolExecutor.map and Pool.map do not 
-                # satisfy the task since they collect iterable immediately.
-                for data_el in tqdm(executor.imap(self._get_data_from_json_line, json_file, chunksize = chunksize), total = total):
-                    if set_gnames:
-                        self.grid_name_list.append(data_el[3])
-                    if transform is not None:
-                        data_el = transform(data_el)
-                    data_list.append(data_el)
-
-        return data_list
-
-
-
-class CurveDatasetSubset:
-    def __init__(self, dataset: CurveDataset, grid_name: str):
+class SwipeDatasetSubset:
+    def __init__(self, dataset: SwipeDataset, grid_name: str):
         assert hasattr(dataset, 'grid_name_list'), \
             "Dataset doesn't have grid_name_list property. " \
             "To fix this create the dataset with store_gnames=True"
@@ -211,29 +167,14 @@ class CurveDatasetSubset:
 
 
 
-class CollateFnV2:
+class CollateFn:
     def __init__(self, batch_first: bool, word_pad_idx: int, 
                  swipe_pad_idx: int = 0) -> None:
         self.word_pad_idx = word_pad_idx
         self.batch_first = batch_first
         self.swipe_pad_idx = swipe_pad_idx
 
-    def _assert_encoder_in_type_and_shape(self, encoder_in_example):
-        assert len(encoder_in_example) == 2
-        for el in encoder_in_example:
-            assert isinstance(el, torch.Tensor), \
-                f"Expected torch.Tensor, got {type(el)}"
-        
-    def _is_encoder_input_tuple(self, batch):
-        encoder_in_example = batch[0][0][0]
-        if isinstance(encoder_in_example, tuple):
-            self._assert_encoder_in_type_and_shape(encoder_in_example)
-            return True
-        elif isinstance(encoder_in_example, torch.Tensor):
-            return False
-        else:
-            raise ValueError(f"Unknown type of encoder input {type(batch[0][0])}")
-    
+
     def __call__(self, batch: list):
         """
         Given a List where each row is 
@@ -253,66 +194,69 @@ class CollateFnV2:
 
         Returns:
         --------
-        transformer_in: tuple of torch tensors:
-            1. (enc_in, dec_in, swipe_pad_mask, word_pad_mask),
-                where enc_in can be either a single tensor or a tuple
-                of two tensors (depends on type of input)
-                Each element is a torch tensor of shape:
-                - enc_in: either (curve_len, batch_size, n_feats) or
-                    ((curve_len, batch_size, n_feats1), (curve_len, batch_size, n_feats2))
-                - dec_in: (chars_seq_len - 1, batch_size)
-                - swipe_pad_mask: (batch_size, curve_len)
-                - word_pad_mask: (batch_size, chars_seq_len - 1, )
+        1. transformer_in: tuple of torch tensors:
+            (enc_in, dec_in, swipe_pad_mask, word_pad_mask),
+            where enc_in can be either a single tensor or a tuple
+            of two tensors (depends on type of input)
+            Each element is a torch tensor of shape:
+            - enc_in: list of tuples of tensors with shapes:
+                [(curve_len, batch_size, n_feats1), (curve_len, batch_size, n_feats2), ...]
+            - dec_in: (chars_seq_len - 1, batch_size)
+            - swipe_pad_mask: (batch_size, curve_len)
+            - word_pad_mask: (batch_size, chars_seq_len - 1)
+        2. dec_out: torch tensor of shape (chars_seq_len - 1, batch_size)
         """
-        is_encoder_input_tuple = self._is_encoder_input_tuple(batch)
-        dec_in_no_pad = []
-        dec_out_no_pad = []
+        decoder_inputs, decoder_outputs = [], []
 
-        encoder_in_no_pad = ([], []) if is_encoder_input_tuple else []
+        num_encoder_features = len(batch[0][0])
+        encoder_inputs = [[] for _ in range(num_encoder_features)]
 
         for row in batch:
-            x_smpl, decoder_out_smpl = row
-            encoder_in_smpl, decoder_in_smpl = x_smpl
-            if is_encoder_input_tuple:
-                for i in range(2):
-                    encoder_in_no_pad[i].append(encoder_in_smpl[i])
-            else:
-                encoder_in_no_pad.append(encoder_in_smpl)
+            (enc_in, dec_in), dec_out = row
 
-            dec_in_no_pad.append(decoder_in_smpl)
-            dec_out_no_pad.append(decoder_out_smpl)
+            for feature, features_list in zip(enc_in, encoder_inputs):
+                features_list.append(feature)
 
-        if is_encoder_input_tuple:
-            encoder_in = tuple(pad_sequence(encoder_in_no_pad_i, batch_first=self.batch_first, 
-                                       padding_value=self.swipe_pad_idx)
-                          for encoder_in_no_pad_i in encoder_in_no_pad)
-        else:
-            encoder_in = pad_sequence(encoder_in_no_pad, batch_first=self.batch_first,
-                                      padding_value=self.swipe_pad_idx)
+            decoder_inputs.append(dec_in)
+            decoder_outputs.append(dec_out)
 
-        dec_out = pad_sequence(dec_out_no_pad, batch_first=self.batch_first,
-                                        padding_value=self.word_pad_idx)
+        encoder_inputs_padded = [
+            pad_sequence(
+                encoder_in_el, batch_first=self.batch_first,
+                padding_value=self.swipe_pad_idx)
+            for encoder_in_el in encoder_inputs]
+
+        decoder_inputs_padded = pad_sequence(
+            decoder_inputs, batch_first=self.batch_first,
+            padding_value=self.word_pad_idx)
         
-        dec_in = pad_sequence(dec_in_no_pad, batch_first=self.batch_first,
-                                        padding_value=self.word_pad_idx)
+        decoder_outputs_padded = pad_sequence(
+            decoder_outputs, batch_first=self.batch_first,
+            padding_value=self.word_pad_idx)
         
-        word_pad_mask = dec_in == self.word_pad_idx
+
+        word_pad_mask = decoder_inputs_padded == self.word_pad_idx
         if not self.batch_first:
             word_pad_mask = word_pad_mask.T  # word_pad_mask is always batch first
 
-        encoder_in_el = encoder_in[0] if is_encoder_input_tuple else encoder_in
+
+        encoder_in_el = encoder_inputs_padded[0]
         max_curve_len = encoder_in_el.shape[1] if self.batch_first else encoder_in_el.shape[0]
-        encoder_in_no_pad_el = encoder_in_no_pad[0] if is_encoder_input_tuple else encoder_in_no_pad
-        encoder_lens = torch.tensor([len(x) for x in encoder_in_no_pad_el])
+        encoder_inputs_single_feature_no_pad = encoder_inputs[0]
+        encoder_lens = torch.tensor([len(x) for x in encoder_inputs_single_feature_no_pad])
 
         # Берем матрицу c len(encoder_lens) строками вида
         # [0, 1, ... , max_curve_len - 1].  Каждый элемент i-ой строки
         # сравниваем с длиной i-ой траектории.  Получится матрица, где True
         # только на позициях, больших, чем длина соответствующей траектории.
         # (batch_size, max_curve_len)
-        encoder_pad_mask = torch.arange(max_curve_len).expand(
+        swipe_pad_mask = torch.arange(max_curve_len).expand(
             len(encoder_lens), max_curve_len) >= encoder_lens.unsqueeze(1)
         
-        transformer_in = (encoder_in, dec_in, encoder_pad_mask, word_pad_mask)
+
+        transformer_in = (encoder_inputs_padded, 
+                          decoder_inputs_padded, 
+                          swipe_pad_mask, 
+                          word_pad_mask)
         
-        return transformer_in, dec_out
+        return transformer_in, decoder_outputs_padded
