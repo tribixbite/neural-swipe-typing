d6d75a4b33f2aacf86e332e1694eec971ed7ff29
Garry Proshian <98213116+proshian@users.noreply.github.com>
2025-05-20 17:18:32 +0300

    Add tests for OnlineMeanStd

--- src/unittests/test_distance_getter.py
+++ src/unittests/test_distance_getter.py
@@ -3,9 +3,9 @@ import sys; import os; sys.path.insert(1, os.path.join(os.getcwd(), "src"))
 
 import unittest
 
-import numpy as np
+import torch
 
-from feature_extraction.distances_lookup import distance
+from feature_extraction.distance_getter import compute_pairwise_distances
 
 
 class TestDistance(unittest.TestCase):
@@ -17,35 +17,35 @@ class TestDistance(unittest.TestCase):
         N_KEYS = 30
         WIDTH = MAX_COORD = 1080
         HEIGHT = 667
-        dots = np.indices((WIDTH, HEIGHT)).transpose(1, 2, 0)  # (WIDTH, HEIGHT, 2)
-        centers = np.random.randint(0, MAX_COORD, (N_KEYS, 2))
-        result = distance(dots, centers)
+        dots = torch.stack(torch.meshgrid(torch.arange(WIDTH), torch.arange(HEIGHT), indexing='xy'), dim=-1)  # (WIDTH, HEIGHT, 2)
+        centers = torch.randint(0, MAX_COORD, (N_KEYS, 2))
+        result = compute_pairwise_distances(dots, centers)
         self.assertEqual(result.shape, (WIDTH, HEIGHT, N_KEYS))
 
     def test_distance_shape_2(self):
         N_KEYS = 30
         MAX_COORD = 1080
         DOT_DIMS = (100, 50, 90)
-        dots = np.random.randint(0, MAX_COORD, (*DOT_DIMS, 2))
-        centers = np.random.randint(0, MAX_COORD, (N_KEYS, 2))
-        result = distance(dots, centers)
+        dots = torch.randint(0, MAX_COORD, (*DOT_DIMS, 2))
+        centers = torch.randint(0, MAX_COORD, (N_KEYS, 2))
+        result = compute_pairwise_distances(dots, centers)
         self.assertEqual(result.shape, (*DOT_DIMS, N_KEYS))
 
     def testcase_dots_1(self):
-        dots = np.array([[1, 2], [3, 4], [5, 6]])
-        centers = np.array([[1, 2], [3, 4]])
-        result = distance(dots, centers)
-        expected = np.array([[0, 8], [8, 0], [32, 8]])
-        self.assertTrue(np.allclose(result, expected))
+        dots = torch.tensor([[1, 2], [3, 4], [5, 6]])
+        centers = torch.tensor([[1, 2], [3, 4]])
+        result = compute_pairwise_distances(dots, centers)
+        expected = torch.tensor([[0, 8], [8, 0], [32, 8]])
+        self.assertTrue(torch.allclose(result, expected))
 
     def testcase_grid_1(self):
         WIDTH = 2
         HEIGHT = 3
-        dots = np.indices((WIDTH, HEIGHT)).transpose(1, 2, 0)
-        centers = np.array([[4, 6]])
-        result = distance(dots, centers)
-        expected = np.array([[[52], [41], [32]], [[45], [34], [25]]])
-        self.assertTrue(np.allclose(result, expected))
+        dots = torch.stack(torch.meshgrid(torch.arange(WIDTH), torch.arange(HEIGHT), indexing='xy'), dim=-1)
+        centers = torch.tensor([[4, 6]])
+        result = compute_pairwise_distances(dots, centers)
+        expected = torch.tensor([[[52], [41], [32]], [[45], [34], [25]]])
+        self.assertTrue(torch.allclose(result, expected))
 
 
 if __name__ == '__main__':
