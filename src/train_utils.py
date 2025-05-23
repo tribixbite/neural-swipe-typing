cb7431a2b5797814c9c80995fbbee308350cf4a0
Garry Proshian <98213116+proshian@users.noreply.github.com>
2025-05-24 00:34:44 +0300

    Add CrossEntropyLossWithReshape

---
+++ src/train_utils.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn.functional as F
+
+
+class CrossEntropyLossWithReshape(torch.nn.Module):
+    def __init__(self, ignore_index=-100, label_smoothing=0.0):
+        super().__init__()
+        self.ignore_index = ignore_index
+        self.label_smoothing = label_smoothing
+
+    def forward(self, pred, target):
+        """
+        pred - (D1, D2, ..., Dn, C)
+        target - (D1, D2, ..., Dn)
+        C - number of classes
+        """
+        pred_flat = pred.view(-1, pred.shape[-1])  # D1*D2*...*Dn x C
+        target_flat = target.reshape(-1)  # D1*D2*...*Dn
+        return F.cross_entropy(pred_flat,
+                            target_flat,
+                            ignore_index=self.ignore_index,
+                            label_smoothing=self.label_smoothing)
