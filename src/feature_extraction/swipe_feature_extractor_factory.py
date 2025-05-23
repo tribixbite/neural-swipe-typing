f6a28033df38ccff6ed5d00e1aef789e376eb19b
Garry Proshian <98213116+proshian@users.noreply.github.com>
2025-05-23 21:51:13 +0300

    Complete feature extractor factory

--- src/feature_extraction/swipe_feature_extractor_factory.py
+++ src/feature_extraction/swipe_feature_extractor_factory.py
@@ -1,73 +1,108 @@
 from typing import List
 
-from .swipe_feature_extractors import (SwipeFeatureExtractor, 
-                                       MultiFeatureExtractor,
-                                       TrajectoryFeatureExtractor,
-                                        NearestKeyFeatureExtractor,
-                                        DistancesFeatureExtractor,
-                                        KeyWeightsFeatureExtractor)
-from .nearest_key_lookup import NearestKeyLookup
-from .distances_lookup import DistancesLookup
+import torch
+
+from feature_extraction.swipe_feature_extractors import (
+    MultiFeatureExtractor,
+    TrajectoryFeatureExtractor,
+    CoordinateFunctionFeatureExtractor,
+)
+from ns_tokenizers import KeyboardTokenizer
+from feature_extraction.nearest_key_getter import NearestKeyGetter
+from feature_extraction.key_weights_getter import KeyWeightsGetter
+from feature_extraction.normalizers import (
+    MinMaxNormalizer,
+    MeanStdNormalizer,
+)
+from feature_extraction.grid_lookup import GridLookup
+from feature_extraction.key_weights_functions import (WeightsFnV1,
+                                                      WeightsFunctionV1Softmax,  
+                                                      WeightsFunctionSigmoidNormalizedV1)
 
 
-def multi_swipe_feature_extractor_factory(
-    extractor_params_list: List[dict]) -> MultiFeatureExtractor:
-    """
-    Arguments:
-    ----------
-    extractor_params_list : List[dict]
-        List of dictionaries with parameters for each extractor.
-        Each dictionary should contain the following keys:
-        - 'type': type of the extractor (e.g. 'trajectory', 'nearest_key', etc.)
-        - 'params': parameters for the extractor.
-    """
+KEY_WEIGHTS_FUNCTIONS = {
+    "v1": WeightsFnV1,
+    "v1_softmax": WeightsFunctionV1Softmax,
+    "sigmoid_normalized_v1": WeightsFunctionSigmoidNormalizedV1,
+}
+
+
+def swipe_feature_extractor_factory(grid: dict,
+                                    keyboard_tokenizer: KeyboardTokenizer,
+                                    trajectory_features_statistics: dict,
+                                    bounding_boxes: dict,
+                                    grid_name: str,
+                                    component_configs: List[dict]):
     extractors = []
-    for extractor_params in extractor_params_list:
-        extractor_type = extractor_params['type']
-        params = extractor_params['params']
-        extractor = single_swipe_feature_extractor_factory(extractor_type, params)
-        extractors.append(extractor)
+    for component_config in component_configs:
+        extractor_type = component_config["type"]
+        extractor_params = component_config["params"]
+
+        if extractor_type == "trajectory":
+            extractors.append(
+                TrajectoryFeatureExtractor(
+                    include_dt=extractor_params["include_dt"],
+                    include_velocities=extractor_params["include_velocities"],
+                    include_accelerations=extractor_params["include_accelerations"],
+                    x_normalizer=MinMaxNormalizer(
+                        bounding_boxes[grid_name]["x_min"],
+                        bounding_boxes[grid_name]["x_max"]
+                    ),
+                    y_normalizer=MinMaxNormalizer(
+                        bounding_boxes[grid_name]["y_min"],
+                        bounding_boxes[grid_name]["y_max"]
+                    ),
+                    dt_normalizer=MeanStdNormalizer(
+                        **trajectory_features_statistics["dt"]
+                    ),
+                    velocity_x_normalizer=MeanStdNormalizer(
+                        **trajectory_features_statistics["velocity_x"]
+                    ),
+                    velocity_y_normalizer=MeanStdNormalizer(
+                        **trajectory_features_statistics["velocity_y"]
+                    ),
+                    acceleration_x_normalizer=MeanStdNormalizer(
+                        **trajectory_features_statistics["acceleration_x"]
+                    ),
+                    acceleration_y_normalizer=MeanStdNormalizer(
+                        **trajectory_features_statistics["acceleration_y"]
+                    )
+                )
+            )   
+        elif extractor_type == "nearest_key":
+            value_fn = NearestKeyGetter(
+                grid=grid,
+                tokenizer=keyboard_tokenizer,
+            )
+            cast_dtype = None
+
+            if extractor_params.get("use_lookup", False):
+                value_fn = GridLookup(grid["width"], grid["height"], value_fn)
+                cast_dtype=torch.int32
+            
+            extractors.append(
+                CoordinateFunctionFeatureExtractor(value_fn, cast_dtype)
+            )
+        elif extractor_type == "key_weights":
+            weights_function_ctor = KEY_WEIGHTS_FUNCTIONS[extractor_params["weights_function"]["function_name"]]
+            weights_function = weights_function_ctor(
+                **extractor_params["weights_function"]["params"]
+            )
+            value_fn = KeyWeightsGetter(
+                grid=grid,
+                tokenizer=keyboard_tokenizer,
+                weights_function=weights_function,
+                missing_value_weight=extractor_params.get("missing_value_weight", 0.0),
+            )
+            cast_dtype = None
+
+            if extractor_params.get("use_lookup", False):
+                value_fn = GridLookup(grid["width"], grid["height"], value_fn)
+                cast_dtype=torch.int32
+            
+            extractors.append(
+                CoordinateFunctionFeatureExtractor(value_fn, cast_dtype)
+            )
+        else:
+            raise ValueError(f"Unknown feature extractor type: {extractor_type}")
     return MultiFeatureExtractor(extractors)
-
-
-def single_swipe_feature_extractor_factory(
-        type: str,
-        params: dict) -> SwipeFeatureExtractor:
-    """
-    Arguments:
-    ----------
-    type : str
-        Type of the extractor (e.g. 'trajectory', 'nearest_key', etc.)
-    params : dict
-        Parameters for the extractor.
-    """
-    if type == 'trajectory':
-        coordinate_normalizer = get_coordinate_normalizer(params['coordinate_normalizer'])
-        dt_normalizer = params.get('dt_normalizer', lambda x: x)
-        velocities_normalizer = params.get('velocities_normalizer', lambda x: x)
-        accelerations_normalizer = params.get('accelerations_normalizer', lambda x: x)
-        return TrajectoryFeatureExtractor(
-            params['include_dt'],
-            params['include_velocities'],
-            params['include_accelerations'],
-            coordinate_normalizer,
-            dt_normalizer,
-            velocities_normalizer,
-            accelerations_normalizer
-        )
-
-    elif type == 'nearest_key':
-        get_keyboard_tokenizer = get_tokenizer(params.get('keyboard_tokenizer_name'))
-        grid = get_grid(params.get('grid_name'))
-        nearest_key_candidates = params.get('nearest_key_candidates')
-        nearest_key_lookup = NearestKeyLookup(grid) 
-        return NearestKeyFeatureExtractor(nearest_key_lookup, get_keyboard_tokenizer)
-    
-    elif type == 'distances':
-        pass
-
-    elif type == 'key_weights':
-        pass
-
-    else:
-        raise ValueError(f"Unknown extractor type: {type}")
