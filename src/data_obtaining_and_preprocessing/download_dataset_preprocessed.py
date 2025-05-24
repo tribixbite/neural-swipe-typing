e3a3d81eb2af2a64d179f5052a4bcbde0293ec4f
Garry Proshian <98213116+proshian@users.noreply.github.com>
2025-05-24 22:24:57 +0300

    Update dataset gdrive link

--- src/data_obtaining_and_preprocessing/download_dataset_preprocessed.py
+++ src/data_obtaining_and_preprocessing/download_dataset_preprocessed.py
@@ -3,7 +3,7 @@ import gdown
 if __name__ == "__main__":
     DATA_PATH = "../data/data_preprocessed"
     
-    url = "https://drive.google.com/drive/folders/1rRBUKUC0D6eZBJqT9qKs5fKQLl-gboej"
+    url = "https://drive.google.com/drive/folders/1V2QxYfxkqHnMM3I-OJjYzlP5AgmMyiAN"
     
     gdown.download_folder(url,
                           output=DATA_PATH,
